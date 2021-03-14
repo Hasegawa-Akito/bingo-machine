f4bbb294f8908246b7375573cdb795ed46083d8f
Hasegawa-Akito <h.akito1999@gmail.com>
2021-03-14 18:42:06 +0900

    add menu

--- bingo.js
+++ bingo.js
@@ -10,7 +10,9 @@ function numMax(Max_num){
     return bingo_num;
 }
 //console.log(numMax(75));
-var bingo_num=numMax(75);
+//マックス値の初期値75を入れておく
+var Max_num=75;
+var bingo_num=numMax(Max_num);
 
 
 //変数のデバッグに使う関数
@@ -28,14 +30,22 @@ audio_drm.src="music/doramuroll.mp3";
 var audio_kti=new Audio();
 audio_kti.src="music/ketteionn.mp3";
 
+var display_cout=0;
+//ビンゴスタート後maxの値を変えれないようにするため
+var start_count=0;
+
 var app=new Vue({
     el:"#app",
     data:{
         num:initial,
-        records:record_num
+        records:record_num,
+        max:75,
+        menu_name:"設定",
+        displayChange:"none",
     },
     methods:{
         start:function(){
+            start_count=1;
             //数字が変わる演出
             audio_kti.pause();
             audio_kti.currentTime=0;
@@ -56,6 +66,34 @@ var app=new Vue({
             var delete_index=bingo_num.indexOf(this.num);
             bingo_num.splice(delete_index,1);
             //dev_console(bingo_num);
+        },
+        settingDisplay:function(){
+            if(display_cout==0){
+                this.displayChange="block";
+                this.menu_name="閉じる";
+                display_cout=1;
+            }else{
+                this.displayChange="none";
+                this.menu_name="設定";
+                display_cout=0;
+            }
+        },
+        maxInput:function(){
+            if(isNaN(this.max)){
+                alert("半角の数値を入力してください");
+                this.max=Max_num;
+            }
+            if(start_count==0){
+                Max_num=this.max;
+                bingo_num=numMax(Max_num);
+                dev_console(this.max);
+                //dev_console(bingo_num);
+            }else{
+                alert("ビンゴスタート後には変更できません");
+                this.max=Max_num;
+            }
+            
         }
+        
     }
 });
